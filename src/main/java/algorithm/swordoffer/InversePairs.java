fcc2beee1221d05d41842a5ff827f0b1bd082888
zhangqian <zhangqian13@le.com>
2018-12-17 17:04:21 +0800

    剑指offer第五章

---
+++ src/main/java/algorithm/swordoffer/InversePairs.java
@@ -0,0 +1,29 @@
+package algorithm.swordoffer;
+
+/**
+ * @Author: evilhex
+ * @Date: 2018-12-17 16:23
+ */
+public class InversePairs {
+
+    // 这种方案超时
+    static public int InversePairs(int[] array) {
+        if (array == null || array.length <= 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 0};
+        System.out.println(InversePairs(arr));
+    }
+}
